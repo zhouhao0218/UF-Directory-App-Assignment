148b2812b39065e77ceabe72d056c2fca6578de7
zhouhao0218 <zhouhao0218@ufl.edu>
2018-09-10 18:56:33 -0400

    Assignment2 completed

--- listingController.js
+++ listingController.js
@@ -1,14 +1,44 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+	  $scope.newItem = {
+		coordinates: {
+			latitude: 0,
+			longitude: 0
+		  }
+	   };
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+	  var clear = function(){
+		     $scope.newItem.name = '';
+		     $scope.newItem.code = '';
+		     $scope.newItem.address = '';
+		     $scope.newItem.coordinates.latitude = 0;
+		     $scope.newItem.coordinates.longitude = 0;
+    };
+
+	  clear();
+
+    $scope.addListing = function() {
+		var addNewItem = {
+			name: $scope.newItem.name,
+			code: $scope.newItem.code,
+			address: $scope.newItem.address,
+			coordinates: {
+				latitude: $scope.newItem.coordinates.latitude,
+				longitude: $scope.newItem.coordinates.longitude
+			}
+		}
+		$scope.listings.push(addNewItem);
+		clear();
+	  };
+
+    $scope.deleteListing = function(index) {
+		$scope.listings.splice(index, 1);
+	  };
+
+    $scope.showDetails = function(listing) {
+		$scope.detailedInfo = $scope.listings[$scope.listings.indexOf(listing)];
+	  };
   }
 ]);
